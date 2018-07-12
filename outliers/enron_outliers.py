dc3c6645e73c631ced6c19a75a21ef884dd39dbb
Michael White <michael@6whites.com>
2018-07-12 15:45:54 -0700

    Finished outliers and saved out all changes

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -9,11 +9,27 @@ from feature_format import featureFormat, targetFeatureSplit
 
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+data_dict.pop("TOTAL",0)
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
+for key , values in data_dict.items():
+    salary=float(values.get("salary"))
+    bonus=float(values.get("bonus"))
+    if(salary>1000000 and bonus>=5000000):
+        print(key)
 
 ### your code below
 
 
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
+maxBonus = 0
+itemCap = ""
+index = 0
 
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
