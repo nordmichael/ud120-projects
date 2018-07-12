0d450bc87fc4d78e2f30d9663f7bdc649af07329
Michael White <michael@6whites.com>
2018-07-12 09:06:57 -0700

    Part-way commit on Lesson 5.17

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-""" 
+"""
     Starter code for exploring the Enron dataset (emails + finances);
     loads up the dataset (pickled dict of dicts).
 
@@ -12,11 +12,33 @@
     but here's an example to get you started:
 
     enron_data["SKILLING JEFFREY K"]["bonus"] = 5600000
-    
+
 """
 
 import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+#Count total number of people in dataset
+print "Number of People: ", len(enron_data)
 
+#print enron_data.viewkeys()
+
+#Get total intersect of dataset and people of interest
+poi_count = 0
+for person in enron_data:
+    if enron_data[person]["poi"] == True:
+        poi_count += 1
+print poi_count
+
+
+#Get person of interest names from poi names file
+#Discard first line because it's a web address
+f_poi_names = open("../final_project/poi_names.txt",'r')
+f_poi_names.readline()
+count_poi_names = 0
+for name in f_poi_names:
+    if len(name) > 1:
+        #print "Name: ", name
+        count_poi_names += 1
+print count_poi_names
